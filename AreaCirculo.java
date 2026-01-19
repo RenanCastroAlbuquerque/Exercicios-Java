50f2d873a0ef6da72a976329c1fed7ab4f47eeeb
Renan de Castro albuquerque <167430702+RenanCastroAlbuquerque@users.noreply.github.com>
2026-01-19 19:32:59 -0300

    Add files via upload

---
+++ AreaCirculo.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class AreaCirculo {
+    public static void main (String[] args){
+        // Calculando a area do circulo circulo
+        final double PI = 3.14;
+        int raio = 44;
+        double result = PI * (raio * raio);
+
+        System.out.println("Resultado: " + result);
+    }
+}
---
+++ ConverterTemperatura.java
@@ -0,0 +1,10 @@
+package Exercicios;
+
+public class ConverterTemperatura {
+    public static void main(String[] args){
+        // Fahreinhait para Celsius
+        double fah = 178;
+        double resultado = (fah - 32) * 5/9;
+        System.out.println("Resultado = " + resultado + " Celsius");
+    }
+}
