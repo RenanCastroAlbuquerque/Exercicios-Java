b34f282ede4d22050f27f3d4643fc4e85b43900b
Renan de Castro albuquerque <167430702+RenanCastroAlbuquerque@users.noreply.github.com>
2026-01-21 18:12:49 -0300

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Qual sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Nome = " + nome + "\t \n idade = " + idade);
+        scanner.close();
+    }
+}
